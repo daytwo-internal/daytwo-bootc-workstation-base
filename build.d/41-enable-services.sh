f39a9f241575aca9f3063c303a317bedf8933b66
Ricardo Arguello <ricardo.arguello@gmail.com>
2026-04-19 19:37:35 +0200

    fix: Flatpack preinstall as root

--- build.d/41-enable-services.sh
+++ build.d/41-enable-services.sh
@@ -4,11 +4,11 @@ set -euxo pipefail
 systemctl enable \
     bootc-fetch-apply-updates.timer \
     cockpit.socket \
+    flatpak-preinstall.service \
     libvirtd.socket \
     zerotier-one.service
 
 systemctl --global enable \
-    flatpak-preinstall.service \
     flatpak-update.timer
 
 systemctl set-default graphical.target
