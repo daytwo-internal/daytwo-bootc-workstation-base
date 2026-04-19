2db37ba99a734d77bd96108e7e0778ed62399a8a
Ricardo Arguello <ricardo.arguello@gmail.com>
2026-04-19 12:27:04 +0200

    feat: Enable auto updates

--- build.d/41-enable-services.sh
+++ build.d/41-enable-services.sh
@@ -2,8 +2,10 @@
 set -euxo pipefail
 
 systemctl enable \
+    bootc-fetch-apply-updates.timer \
     cockpit.socket \
     flatpak-preinstall.service \
+    flatpak-update.timer \
     libvirtd.socket \
     zerotier-one.service
 
